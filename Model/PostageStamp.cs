8c305ca039dd724409c89133b11b7eca6e5cf293
Mat <123@changes.com>
2024-02-23 11:07:13 +0100

    first iteration

---
+++ Model/PostageStamp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scrapbook.Model
+{
+    class PostageStamp
+    {
+        private int Id {  get; set; }
+        public string Name { get; set; }
+        public DateTime IssueDate { get; set; }
+        public Country Country { get; set; }
+        public StampTopic StampTopic { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Scrapbook.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scrapbook.ViewModel
+{
+    class MainWindowViewModel
+    {
+        public List<PostageStamp> PostageStamps { get; private set; } = new List<PostageStamp>();
+        public MainWindowViewModel() 
+        {
+            
+        }
+    }
+}
